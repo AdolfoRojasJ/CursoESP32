ffe84e74c610dce3fd8426b48b2e0cb4591565d1
AdolfoRojasJ <adolfor202020@gmail.com>
2026-01-16 11:43:36 -0600

    Códigos Hasta Día 5/10.

--- Dia 5/RGB_Bluetooth/RGB_Bluetooth.ino
+++ Dia 5/RGB_Bluetooth/RGB_Bluetooth.ino
@@ -1,21 +1,21 @@
-//ESP32 - PWM - UART(Serial)
-//Controlar la intencidad del led de la tarjeta 
-
-//Variables y constantes 
+// Control RGB por Bluetooth
+#include <BluetoothSerial.h>;
+BluetoothSerial bt;
+String s="";
 //Color rojo (R)
 #define R_led 25   //Dirección del led
 #define R_fre 1000   //Frecuencia PWM
-#define R_res 16   //Resoliución PEM
+#define R_res 16   //Resolución PEM
 #define R_Max (pow(2,R_res)-1)  //El valor maximo de ancho de pulso 
 //Color verde (G)
 #define G_led 15   //Dirección del led
 #define G_fre 1000   //Frecuencia PWM
-#define G_res 16   //Resoliución PEM
+#define G_res 16   //Resolución PEM
 #define G_Max (pow(2,G_res)-1)  //El valor maximo de ancho de pulso 
 //Color azul (B)
 #define B_led 4   //Dirección del led
 #define B_fre 1000   //Frecuencia PWM
-#define B_res 16   //Resoliución PEM
+#define B_res 16   //Resolución PEM
 #define B_Max (pow(2,B_res)-1)  //El valor maximo de ancho de pulso 
 
 unsigned int R_ap=0; //Ancho de pulso 
@@ -26,6 +26,8 @@ void setup() {
   //Serial
   Serial.begin(115200);
   delay(200);
+  bt.begin("Hy3n4");
+  delay(500);
   //GPIO
   pinMode(R_led,OUTPUT);  //Cuando es PWM es opcional asignarlo como salida
   pinMode(G_led,OUTPUT);  //Cuando es PWM es opcional asignarlo como salida
@@ -36,15 +38,11 @@ void setup() {
   ledcAttach(B_led,B_fre,B_res); //La salida es led, la frecuencia es 1KHz y la resolución de 16 bits
 
   Serial.println();
-  Serial.println("Control de un led RGB por comunicación en Serie");
+  Serial.println("Control de un led RGB por Bluetooth");
   Serial.println();
 }
 
-void loop() {
-  if(Serial.available()){
-    String s=Serial.readStringUntil('\n');
-    Serial.println("Recibido -> " + s);
-
+void decod(){
     int posPts =s.indexOf(':'); //Posición de los :
     int posFin = s.length();  //Largo de la cadena 
     String c = s.substring(0,posPts);
@@ -58,10 +56,23 @@ void loop() {
     else if(c.equals("B"))  B_ap=map(pwm,0,100,0,B_Max);
 
     s = "Colores -> R: " +  String(R_ap) +
-                   "   G: " +  String(G_ap) +
-                   "   B: " +  String(B_ap);
+    "   G: " +  String(G_ap) +
+    "   B: " +  String(B_ap);
+}
+void loop() {
+  if(Serial.available()){
+    s=bt.readStringUntil('\n');
+    Serial.println("Recibido -> " + s);
+    decod();
     Serial.println(s);
   }
+  if(bt.available()){
+    s=bt.readStringUntil('\n');
+    bt.println("Recibido -> " + s);
+    decod();
+    bt.println(s);
+  }
+  s="";
   ledcWrite(R_led,R_ap);
   ledcWrite(G_led,G_ap);
   ledcWrite(B_led,B_ap);
